1f33a35220d6363b8e623354f10814e4fb302184
anthony anup <anup>
2025-07-20 12:16:24 +0530

    display todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { IoSend } from "react-icons/io5";
 
-
-
 const App = () => {
+  const [formdata, setformdata] = useState("");
 
-  const [todos, settodos] = useState([])
+  const [todos, settodos] = useState([]);
+
+  const submit = (e) => {
+    e.preventDefault();
+    setformdata(formdata);
+    console.log(formdata);
+    settodos([...todos, formdata]);
+    setformdata("");
+    console.log(todos);
+  };
   return (
     <div>
-
-      <div className='flex justify-center text-9xl font-bold opacity-30 mt-30'>
+      <div className="flex justify-center text-9xl font-bold opacity-30 mt-30">
         ToDoS
-
       </div>
 
+      <div className="flex justify-center mt-20 ">
+        <form onSubmit={submit}>
+          <div className="relative">
+            <input
+              type="text"
+              value={formdata}
+              onChange={(e) => setformdata(e.target.value)}
+              className="w-[800px] h-[60px] rounded-3xl text-2xl p-5 shadow-[0_4px_15px_rgba(0,0,0,0.5)] pl-8 pr-13"
+              placeholder="Add Todo....."
+            />
+            <button type="submit">
+              <IoSend
+                size={40}
+                className="absolute top-0 left-187 mt-2 hover:opacity-50"
+              />
+            </button>
+          </div>
+        </form>
+      </div>
 
-      <div className='flex justify-center mt-20 '>
-        <div className='relative'>
-          <input type="text" className='w-[800px] h-[60px] rounded-3xl text-2xl p-5 shadow-[0_4px_15px_rgba(0,0,0,0.5)] pl-8 pr-13' placeholder='Add Todo.....' />
-          <IoSend size={40} className='absolute top-0 left-187 mt-2 hover:opacity-50' />
-
+      <div className="flex justify-center mt-10 overflow-auto h-[450px]">
+        <div>
+          {todos.map((data) => (
+            <div className="relative">
+              <input
+                type="text"
+                className=" w-[780px] h-[60px]  text-3xl px-10 "
+                value={data}
+                disabled
+              />
+              <MdDelete
+                size={37}
+                className="absolute top-0 left-189 mt-2 hover:opacity-50"
+              />
+              <input
+                type="checkbox"
+                className=" absolute top-0 left-0 w-5 h-5 mt-5 "
+              />
+            </div>
+          ))}
         </div>
-
-
-
-
       </div>
-
-
-      <div className='flex justify-center mt-10 '>
-        <div className='relative'>
-          <input type="text" className=' w-[780px] h-[60px]  text-3xl px-10 ' value="hello wrold" disabled />
-          <MdDelete size={43} className='absolute top-0 left-189 mt-2 hover:opacity-50' />
-          <input type="checkbox" className=' absolute top-0 left-0 w-5 h-5 mt-5 ' />
-        </div>
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
